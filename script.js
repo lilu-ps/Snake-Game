9426ac14f7d49ec53786743d7eff1a98549498c1
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2020-04-29 01:37:24 +0400

    snake body moves

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 import * as CONSTANTS from "./config.js";
 import { Snake } from "./snake.js";
+let snake ;
 
-let snake = new Snake(document.getElementById("rect"));
+// init();
 
-window.onload = setInterval(function(){
-    snake.move();
-}, 100);
+window.onload = init();
+
+function init(){
+    drawSnake();
+    setInterval(function(){
+        moveSnakes();
+     }, 100);
+}
 
 onkeydown = changeSnakeDirection;
 
 function changeSnakeDirection(e){
     var key_code=e.which||e.keyCode;
-    console.log(key_code);
     snake.changeDirection(key_code);
 }
+
+
+function drawSnake(){
+    snake = new Snake();
+    snake.initSnakeBody();
+    let arr = snake.getSnakeBody();
+    let length = snake.getSnakeLength();
+    for (var i = 0; i < length; i++){
+        let curRect = document.createElement('div');
+        curRect.setAttribute('id', i);
+        curRect.setAttribute('class', 'rect');
+        curRect.style.left = CONSTANTS.RECT_WIDTH * arr[i].x + 'px';
+        curRect.style.top = arr[i].y * CONSTANTS.RECT_HEIGHT + 'px';
+        document.getElementById("canvas-id").append(curRect);
+
+    }
+     
+}
+
+function moveSnakes(){
+    let length = snake.getSnakeLength();
+    for (var i = 0; i < length; i++){
+       snake.move(document.getElementById(i), i);
+    }
+}
+
--- snake.js
+++ snake.js
@@ -1,78 +1,131 @@
 import * as CONSTANTS from "./config.js";
 
 export class Snake{
-    constructor(elem) {
-        this.elem = elem;
+    constructor() {
         this.lastDirection = CONSTANTS.RIGHT_DIRECTION;
+
         this.snakeLength = CONSTANTS.SNAKE_DEFAULT_LENGTH;
+        this.snakeBody = [];
+        this.a = 5;
+        this.prev = {x:0, y:0};
+    }
+
+    getLastDirection(){
+        return this.lastDirection;
     }
 
 
-    async getSnakeElem(){
+    getSnakeElem(){
         return this.elem;
     }
 
+    getSnakeBody(){
+        return this.snakeBody;
+    }
 
-    async move(){
+    getA(){
+        return this.a;
+    }
+
+    getSnakeLength(){
+        return (this.snakeLength);
+    }
+
+    setSnakeLength(newLength){
+        this.snakeLength = newLength;
+    }
+
+    initSnakeBody(){
+        for (var i = this.snakeLength - 1; i >= 0; i--){
+            var dict = {x: i, y: 0};
+            this.snakeBody.push(dict);
+        }
+    }
+
+    move(elem, index){
+        let x = this.prev.x;
+        let y = this.prev.y;
+
+        this.prev.x = this.snakeBody[index].x;
+        this.prev.y = this.snakeBody[index].y;
+        if (index == 0){
+           this.moveSnakeHead(elem);
+        } else {
+            this.snakeBody[index].x = x;
+            this.snakeBody[index].y = y;
+        }
+        elem.style.left = this.snakeBody[index].x * CONSTANTS.RECT_WIDTH + 'px';
+        elem.style.top = this.snakeBody[index].y * CONSTANTS.RECT_WIDTH + 'px';
+        
+    }
+ 
+
+    moveSnakeHead(elem){
         switch(this.lastDirection){
             case CONSTANTS.RIGHT_DIRECTION:
-                this._moveHorizontal(1, this.elem);
+                this._moveHorizontal(1, elem);
                 break;
             case CONSTANTS.LEFT_DIRECTION:
-                this._moveHorizontal(-1, this.elem);
+                this._moveHorizontal(-1, elem);
                 break;
             case CONSTANTS.DOWN_DIRECTION:
-                this._moveVertical(1, this.elem);
+                this._moveVertical(1, elem);
                 break;
             case CONSTANTS.UP_DIRECTION:
-                this._moveVertical(-1,this.elem);
+                this._moveVertical(-1, elem);
                 break;
         }
     }
-    
+
+
     /*
      * Moves in horizontal direction. If direction equals -1, the snake moves
      * left, if direction equals 1, then it moves right.
     */
-    async _moveHorizontal(direction, elem){
+    _moveHorizontal(direction, elem){
         if (direction == 1 && elem.offsetLeft + CONSTANTS.RECT_WIDTH == CONSTANTS.CANVAS_WIDTH) return;
         if (direction == -1 && elem.offsetLeft == 0) return;
 
-        elem.style.left=elem.offsetLeft + (direction * CONSTANTS.RECT_WIDTH) + 'px';
+        this.snakeBody[0].x += direction;
     }
-    
+
     /*
      * Moves in vertical direction. If direction equals -1, the snake moves
      * up, if direction equals 1, then it moves down.
     */
-    async _moveVertical(direction, elem){
+    _moveVertical(direction, elem){
         if (direction == 1 && elem.offsetTop + CONSTANTS.RECT_HEIGHT == CONSTANTS.CANVAS_HEIGHT) return;
         if (direction == -1 && elem.offsetTop == 0) return;
 
-        elem.style.top=elem.offsetTop + (direction * CONSTANTS.RECT_HEIGHT) + 'px';
+        //elem.style.top=elem.offsetTop + (direction * CONSTANTS.RECT_HEIGHT) + 'px';
+        this.snakeBody[0].y += direction;
     }
-    
-    
+
+
     /*
-     * Changes direction by assiging last direction to left, right, down or up 
+     * Changes direction by assiging last direction to left, right, down or up
      * direction constants.
      */
-    async changeDirection(key_code){
+    changeDirection(key_code){
         switch(key_code){
             case CONSTANTS.LEFT_CODE:
-                this.lastDirection = CONSTANTS.LEFT_DIRECTION;
+                if (this.lastDirection != CONSTANTS.RIGHT_DIRECTION)
+                    this.lastDirection = CONSTANTS.LEFT_DIRECTION;
                 break;
             case CONSTANTS.UP_CODE:
-                this.lastDirection = CONSTANTS.UP_DIRECTION;
+                if (this.lastDirection != CONSTANTS.DOWN_DIRECTION)
+                    this.lastDirection = CONSTANTS.UP_DIRECTION;
                 break;
             case CONSTANTS.RIGHT_CODE:
-                this.lastDirection = CONSTANTS.RIGHT_DIRECTION;
+                if (this.lastDirection != CONSTANTS.LEFT_DIRECTION)
+                    this.lastDirection = CONSTANTS.RIGHT_DIRECTION;
                 break;
-            case CONSTANTS.DOWN_CODE:
-                this.lastDirection = CONSTANTS.DOWN_DIRECTION;
+            case CONSTANTS.DOWN_CODE:   
+                if (this.lastDirection != CONSTANTS.UP_DIRECTION)
+                    this.lastDirection = CONSTANTS.DOWN_DIRECTION;
                 break;
         }
     }
-    
-    
+
+
 }
